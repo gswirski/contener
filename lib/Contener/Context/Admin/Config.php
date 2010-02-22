34965c7bedc0cee143d5f0bfce96db6b7d3e2dc5
Grzegorz Świrski <sognat@gmail.com>
2010-02-22 20:20:09 +0100

    [Admin Layout] configuration stub

--- lib/Contener/Context/Admin/Config.php
+++ lib/Contener/Context/Admin/Config.php
@@ -4,6 +4,15 @@ class Contener_Context_Admin_Config extends Contener_Component
 {
     function renderHtml()
     {
+        $navigation = new Contener_Navigation(array(
+            array('path' => '/admin/config', 'title' => 'Ogólne'),
+            array('path' => '/admin/config/page_type', 'title' => 'Typy stron'),
+            array('path' => '/admin/config/templates', 'title' => 'Szablony'),
+            array('path' => '/admin/config/plugin', 'title' => 'Wtyczki'),
+            array('path' => '/admin/config/editor', 'title' => 'Edytor wizualny'),
+        ));
+        $this->context->area('left')->addModule('Konfiguruj', $navigation);
+        
         return '<h2>Konfiguracja</h2>';
     }
 }
