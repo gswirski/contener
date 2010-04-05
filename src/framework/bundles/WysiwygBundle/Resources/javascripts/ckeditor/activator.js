86f269e8b2011759a69e56c56f8c2c10a45d8019
Grzegorz Świrski <sognat@gmail.com>
2010-04-05 17:04:38 +0200

    Finishing toolbar in contener skin for ckeditor

--- src/framework/bundles/WysiwygBundle/Resources/javascripts/ckeditor/activator.js
+++ src/framework/bundles/WysiwygBundle/Resources/javascripts/ckeditor/activator.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
+    CKEDITOR.addStylesSet( 'my_styles',
+    [
+        { name : 'Bez formatowania', element : 'p' },
+        { name : 'Nagłówek duży', element : 'h3' },
+        { name : 'Nagłówek średni' , element : 'h4'  },
+        { name : 'Nagłówek mały' , element : 'h5'  }
+    ]);
+    
     var config = {
     	toolbar:
     	[
+        	['-','NumberedList','BulletedList','-','Blockquote','-'],
     	    ['Styles','-','Bold','Italic','Underline','Strike','-'],
-        	['Link','Unlink','Anchor','-'],
         	['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-'],
-        	['NumberedList','BulletedList','-'],
+        	['Link','Unlink','Anchor','-'],
             ['Source']
         ],
+        stylesCombo_stylesSet: 'my_styles',
     	skin: 'contener'
     };
     $('textarea').ckeditor(config);
