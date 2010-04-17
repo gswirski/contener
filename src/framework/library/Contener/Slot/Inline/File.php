acf9759cdea03ca94cfc633aad7142088bb947ec
Grzegorz Świrski <sognat@gmail.com>
2010-04-17 19:41:33 +0200

    Moved file handling logic to more general class (from Slot_Photo to Slot_File)

--- src/framework/library/Contener/Slot/Inline/File.php
+++ src/framework/library/Contener/Slot/Inline/File.php
@@ -2,5 +2,48 @@
 
 class Contener_Slot_Inline_File extends Contener_Slot_Inline
 {
+    protected $file;
     
+    /**
+     * Used when waking from database
+     */
+    public function setFile($file)
+    {
+        $this->file = $file;
+        return $this;
+    }
+    
+    public function getFile()
+    {
+        return $this->file;
+    }
+    
+    /**
+     * Used to point where uploaded file is
+     */
+    public function setValue($value)
+    {
+        if (array_key_exists('tmp_name', $value) && $value['tmp_name']) {
+            if (array_key_exists('name', $value)) {
+                $file = new k_adapter_UploadedFile($value, $this->getId, new k_adapter_DefaultUploadedFileAccess());
+            } else {
+                throw new Exception('Unexpected situation');
+            }
+            
+            $handler = new Contener_Application_Data($file->tmp_name(), Contener_Application_Data::WEB);
+            $handler->copy('uploads/' . $file->name());
+            $this->setFile($file->name());
+        }
+        
+        return parent::setValue(null);
+    }
+    
+    public function editable()
+    {
+        return array_merge(parent::editable(),
+            array(
+                'file' => 'file'
+            )
+        );
+    }
 }
--- src/framework/library/Contener/Slot/Inline/Photo.php
+++ src/framework/library/Contener/Slot/Inline/Photo.php
@@ -2,48 +2,20 @@
 
 class Contener_Slot_Inline_Photo extends Contener_Slot_Inline_File
 {
-    protected $photo;
-    
-    /**
-     * Used when waking from database
-     */
     public function setPhoto($photo)
     {
-        $this->photo = $photo;
-        return $this;
+        return $this->setFile($photo);
     }
     
     public function getPhoto()
     {
-        return $this->photo;
-    }
-    
-    /**
-     * Used to point where uploaded file is
-     */
-    public function setValue($value)
-    {
-        if (array_key_exists('tmp_name', $value) && $value['tmp_name']) {
-            if (array_key_exists('name', $value)) {
-                $file = new k_adapter_UploadedFile($value, $this->getId, new k_adapter_DefaultUploadedFileAccess());
-            } else {
-                throw new Exception('Unexpected situation');
-            }
-            
-            $handler = new Contener_Application_Data($file->tmp_name(), Contener_Application_Data::WEB);
-            $handler->copy('uploads/' . $file->name());
-            $this->setPhoto($file->name());
-        }
-        
-        return parent::setValue(null);
+        return $this->getFile();
     }
     
     public function editable()
     {
-        return array_merge(parent::editable(),
-            array(
-                'photo' => 'photo'
-            )
-        );
+        $editable = parent::editable();
+        $editable['file'] = 'photo';
+        return $editable;
     }
 }
