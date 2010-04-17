d00f4a9944b5f621e0ea42b0923d484a43681714
Grzegorz Świrski <sognat@gmail.com>
2010-04-17 19:45:05 +0200

    Once uploaded file becomes immutable (just for now)

--- src/framework/library/Contener/Resources/slots/Contener_Slot_Inline_File/show.php
+++ src/framework/library/Contener/Resources/slots/Contener_Slot_Inline_File/show.php
@@ -1,2 +1,6 @@
 <label><?php echo $slot->getLabel(); ?></label>
+<?php if ($file = $slot->getFile()) { 
+    echo $file;
+} else { ?>
 <input type="file" name="<?php echo $slot->getFullyQualifiedName(); ?>" id="<?php echo $slot->getId(); ?>" />
+<?php } ?>
