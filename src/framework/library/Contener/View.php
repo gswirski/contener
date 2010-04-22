9debef63fe061fdbfb076fce75da3131d17a67d6
Grzegorz Świrski <sognat@gmail.com>
2010-04-22 21:36:53 +0200

    Removed unnecessary asset-related methods

--- src/framework/library/Contener/View.php
+++ src/framework/library/Contener/View.php
@@ -46,34 +46,6 @@ class Contener_View extends sfTemplateEngine
         return $this->baseUrl;
     }
     
-    public function installStylesheet($name, $path)
-    {
-        return $this->installAsset($name, $path, 'style');
-    }
-    
-    public function uninstallStylesheet($name)
-    {
-        return $this->uninstallAsset($name, 'style');
-    }
-    
-    public function installJavascript($name, $path)
-    {
-        return $this->installAsset($name, $path, 'script');
-    }
-    
-    public function uninstallJavascript($name)
-    {
-        return $this->uninstallAsset($name, 'script');
-    }
-    
-    public function installAsset($name, $path, $namespace = '')
-    {
-        /*$destination = $this->getContainer()->getParameter('loader.base_dir').'/web/'.$namespace.'s/'.$name;
-        
-        $handler = new Contener_Application_Data($path, Contener_Application_Data::WEB);
-        $handler->copy($destination);*/
-    }
-    
     public function installAssets($name, $source = '')
     {
         if (!is_array($name)) {
